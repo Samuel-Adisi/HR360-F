a6cf1bba4776478d637922e6b208340905352cff
Samuel-Adisi <samueladisi734@gmail.com>
2026-04-18 03:13:47 +0000

    tired

--- app/(admin)/(tabs)/recruitment/_layout.jsx
+++ app/(admin)/(tabs)/recruitment/_layout.jsx
@@ -8,7 +8,6 @@ export default function RecruitmentStack() {
       }}
     >
       <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="add" options={{ headerShown: false }} />
     </Stack>
   );
 }
